304643ee5a25d4e494c0d16e44895719128b77d7
Tyler Pardun <tylerpardun@Tylers-MacBook-Pro-3.local>
2020-09-18 16:00:16 -0500

    Added first function

---
+++ R/MATH4753pard0003.R
@@ -0,0 +1,10 @@
+#' Packages
+#'
+#' Contains functions from Lab 4 and beyond.
+#'
+#' @docType package
+#'
+#' @author Tyler Pardun \email{tyler.pardun@ou.edu}
+#'
+#' @name MATH4753pard0003
+NULL
---
+++ R/Package1.R
@@ -0,0 +1,14 @@
+#' Lab 4 function
+#'
+#' @param x is an independent value
+#'
+#' @return yhat value
+#' @export
+#'
+#' @examples
+#' \dontrun{myplot(x=13)}
+#'
+#' @export
+myplot=function(x){
+  0.86089580 +1.46959217*x  -0.02745726*x^2
+}
